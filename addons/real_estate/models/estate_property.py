696164ef20bd87a7ae2e55b60fb6e503a0d1d8b8
hayamahmed2211-oss <hayam.ahmed2211@gmail.com>
2025-12-09 18:02:05 +0200

    modified:   addons/real_estate/models/__pycache__/estate_property.cpython-312.pyc 	modified:   addons/real_estate/models/estate_property.py 	modified:   addons/real_estate/views/property_views.xml modified:   odoo.conf

--- addons/real_estate/models/estate_property.py
+++ addons/real_estate/models/estate_property.py
@@ -1,16 +1,33 @@
+# real_estate/models/property.py
+
 from odoo import models, fields
 
 class RealEstateProperty(models.Model):
     _name = "real.estate.property"
-    _description = "Real estate property"
+    _description = "Real Estate Property"
 
-    name = fields.Char(required=True)
-    description = fields.Text()
-    price = fields.Float()
-    active = fields.Boolean(default=True)
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text(string="Description")
+    postcode = fields.Char(string="Postcode")
+    date_availability = fields.Date(string="Available From")
+    expected_price = fields.Float(string="Expected Price")
+    selling_price = fields.Float(string="Selling Price")
+    bedrooms = fields.Integer(string="Bedrooms")
+    living_area = fields.Integer(string="Living Area (sqm)")
+    facades = fields.Integer(string="Facades")
+    garage = fields.Boolean(string="Garage")
+    garden = fields.Boolean(string="Garden")
+    garden_area = fields.Integer(string="Garden Area (sqm)")
+    garden_orientation = fields.Selection([
+        ('north', 'North'),
+        ('south', 'South'),
+        ('east', 'East'),
+        ('west', 'West')
+    ], string="Garden Orientation")
     state = fields.Selection([
-        ('available','Available'),
-        ('sold','Sold'),
-        ('rented','Rented')
-    ], default='available')
-    partner_id = fields.Many2one('res.partner', string="Owner")
+        ('new', 'New'),
+        ('offer_received', 'Offer received'),
+        ('offer_accepted', 'Offer accepted'),
+        ('sold', 'Sold'),
+        ('cancelled', 'Cancelled')
+    ], string='status', default='new')
