064407f6a6ba19966a5146ad854afbe4677f1b6d
hayamahmed2211-oss <hayam.ahmed2211@gmail.com>
2025-12-09 19:51:17 +0200

    modified:   addons/real_estate/models/__pycache__/estate_property.cpython-312.pyc 	modified:   addons/real_estate/models/estate_property.py modified:   addons/real_estate/views/real_property_views.xml

--- addons/real_estate/models/estate_property.py
+++ addons/real_estate/models/estate_property.py
@@ -1,6 +1,5 @@
-# real_estate/models/property.py
-
 from odoo import models, fields
+from datetime import date, timedelta
 
 class EstateProperty(models.Model):
     _name = "estate.property"
@@ -9,10 +8,26 @@ class EstateProperty(models.Model):
     name = fields.Char(string="Title", required=True)
     description = fields.Text(string="Description")
     postcode = fields.Char(string="Postcode")
-    date_availability = fields.Date(string="Available From")
+    
+    date_availability = fields.Date(
+        string="Available From",
+        default=lambda self: date.today() + timedelta(days=90),  # Default in 3 months
+        copy=False  # Do not copy when duplicating
+    )
+    
     expected_price = fields.Float(string="Expected Price")
-    selling_price = fields.Float(string="Selling Price")
-    bedrooms = fields.Integer(string="Bedrooms")
+    
+    selling_price = fields.Float(
+        string="Selling Price",
+        readonly=True,  # Read-only
+        copy=False      # Do not copy when duplicating
+    )
+    
+    bedrooms = fields.Integer(
+        string="Bedrooms",
+        default=2  # Default number of bedrooms
+    )
+    
     living_area = fields.Integer(string="Living Area (sqm)")
     facades = fields.Integer(string="Facades")
     garage = fields.Boolean(string="Garage")
@@ -24,10 +39,11 @@ class EstateProperty(models.Model):
         ('east', 'East'),
         ('west', 'West')
     ], string="Garden Orientation")
+    
     state = fields.Selection([
         ('new', 'New'),
         ('offer_received', 'Offer received'),
         ('offer_accepted', 'Offer accepted'),
         ('sold', 'Sold'),
         ('cancelled', 'Cancelled')
-    ], string='status', default='new')
+    ], string='Status', default='new')
